eeea5b3df0c23209feabc62366308e1b5d6309b1
Gestin <gestin.remi@gmail.com>
2017-12-02 22:02:43 +0100

    test site

---
+++ application/controllers/Suivi.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Suivi extends CI_Controller {
+    
+        public function __construct() {
+            //	Obligatoire
+            parent::__construct();
+
+            $this->load->model('suivi_model');
+        }
+        
+	public function index()
+	{
+                $data['suivi'] = $this->suivi_model->selectAll();
+		$this->layout->view('suivi/home');
+	}
+}
---
+++ application/views/suivi/home.php
@@ -0,0 +1,25 @@
+<div class="row">
+    <div class="col-md-10 col-md-offset-1 text-center">
+        <table class="table table-bordered" style="font-size: small">
+
+            <tr style ="background-color: #ccccff">
+                <th>numSuivi</th>
+                <th>contacte</th>.
+                <th>annule</th>
+                
+            </tr>
+<?php
+foreach ($suivi as $item) {
+   
+    echo <<<EOT
+    <tr>
+        <td>$item->numSuivi</td>
+        <td>$item->contacte</td>
+        <td>$item->annule</td>
+     </tr>
+EOT;
+}
+?>
+        </table>
+    </div>
+</div>
