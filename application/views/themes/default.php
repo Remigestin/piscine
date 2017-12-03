8cbbbcaea671dc166b004bdf464fbb26900c841e
Gestin <gestin.remi@gmail.com>
2017-12-03 15:48:18 +0100

    mise en place du tableau

--- application/views/themes/default.php
+++ application/views/themes/default.php
@@ -1,34 +1,49 @@
 <!DOCTYPE html>
 <?php ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" > 
+    <head>
+        <meta charset=<?php echo $charset; ?>>
+        <title><?php echo $titre; ?></title>
+        <link href=<?php echo base_url('/assets/css/bootstrap.min.css'); ?> rel="stylesheet">
+        <link href=<?php echo base_url('/assets/css/perso.css'); ?> rel="stylesheet">
+    </head>
 
-    <title>Connexion</title>
 
-    <!-- Bootstrap -->
-    <link href="gentelella-master/vendors/bootstrap/dist/css/bootstrap.min.css"  rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="gentelella-master/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="gentelella-master/vendors/nprogress/nprogress.css" rel="stylesheet">
-    <!-- Animate.css -->
-    <link href="gentelella-master/vendors/animate.css/animate.min.css" rel="stylesheet">
 
-    <!-- Custom Theme Style -->
-    <link href="gentelella-master/build/css/custom.min.css" rel="stylesheet">
-  </head>
+    <body background=<?php //echo base_url('/assets/images/fond.jpg'); ?> >
 
         <header>
-        </header>
-            <div id="contenu">
-                <?php echo $output; ?>
+            <br>
+            <div class="row">
+               
+                                
+                <div class="col-md-1 col-md-offset-7">
+                        
+                    <button type="button" onclick='location.href="<?php echo site_url('');?>"' class="btn btn-default">Accueil</button>
+                    
+                </div>
+               <!--
+                <div class="col-md-1">
+                    <div class="col-md-8 col-md-offset-4">
+                         <button type="button" onclick='location.href="<?php echo site_url('user/logout');?>"' 
+                        class="btn btn-danger">Deconnexion</button>
+                    </div>
+                </div>
+               -->
+                
+                <div class="col-md-2">
+
+                </div>
             </div>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 text-center">
+                    <h1><?php echo $titre; ?></h1>
+                </div>
+            </div>
+        </header>
+        <div id="contenu">
+            <?php echo $output; ?>
+        </div>
 
         <footer class="row">
 
--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,4 +1,28 @@
-<body class="login">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Festival Art et Jeu </title>
+
+    <!-- Bootstrap -->
+    <link href="gentelella-master/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="gentelella-master/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="gentelella-master/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="gentelella-master/vendors/animate.css/animate.min.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="gentelella-master/build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="login">
     <div>
       <a class="hiddenanchor" id="signup"></a>
       <a class="hiddenanchor" id="signin"></a>
@@ -6,8 +30,8 @@
       <div class="login_wrapper">
         <div class="animate form login_form">
           <section class="login_content">
-            <form>
-              <h1>Login Form</h1>
+              <form>
+              <h1>Connexion</h1>
               <div>
                 <input type="text" class="form-control" placeholder="Username" required="" />
               </div>
@@ -15,19 +39,11 @@
                 <input type="password" class="form-control" placeholder="Password" required="" />
               </div>
               <div>
-                <a class="btn btn-default submit" href="index.html">Log in</a>
-                <a class="reset_pass" href="#">Lost your password?</a>
-              </div>
-
-              <div class="clearfix"></div>
+                <a class="btn btn-default submit" href="<?php echo site_url("suivi")?>">Log in</a>
+                <!--<a class="reset_pass" href="#">Lost your password?</a> -->
+              </div>            
 
               <div class="separator">
-                <p class="change_link">New to site?
-                  <a href="#signup" class="to_register"> Create Account </a>
-                </p>
-
-                <div class="clearfix"></div>
-                <br />
 
                 <div>
                   <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
