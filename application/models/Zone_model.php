cd72f157351671df76beb8de4d519ee423969979
michelranari <michel.ranari@gmail.com>
2018-01-13 15:46:14 +0100

    ajout numEditeur

--- application/models/Zone_model.php
+++ application/models/Zone_model.php
@@ -1,39 +1,39 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Zone_model extends CI_Model{
-    
+
  protected $table = 'zone';
- 
+
 public function selectById($id) {
         $this->load->database('default');
-        
+
         return $this->db->select('*')
                         ->from($this->table)
                         ->where('numZone', $id)
                         ->get()
                         ->result();
-                
-    }   
+
+    }
 
 public function getZoneEditeur() {
     $this->load->database('default');
 /*
-select distinct nomEditeur as Editeur, nbDemiTable as "Nombre de 1/2 tables" 
-from(select numZone,numEditeur from zone where zone.numType is NULL) as temp,reservation, editeur 
+select distinct editeur.numEditeur, nomEditeur as Editeur, nbDemiTable as "Nombre de 1/2 tables"
+from(select numZone,numEditeur from zone where zone.numType is NULL) as temp,reservation, editeur
 where reservation.numZone=temp.numZone and temp.NumEditeur=reservation.numEditeur and editeur.numEditeur=reservation.numEditeur
 */
 
-$sql="select distinct nomEditeur , nbDemiTable from(select numZone,numEditeur from zone where zone.numType is NULL) as temp,reservation, editeur 
+$sql="select distinct editeur.numEditeur, nomEditeur , nbDemiTable from(select numZone,numEditeur from zone where zone.numType is NULL) as temp,reservation, editeur 
 where reservation.numZone=temp.numZone and temp.NumEditeur=reservation.numEditeur and editeur.numEditeur=reservation.numEditeur";
 $res=$this->db->query($sql);
 return $res->result() ;
 
 }
-  
+
  public function getZoneType() {
     $this->load->database('default');
 /*
-select libelleType as Type, sum(nbDemiTable) as "Nombre de 1/2 tables" 
+select libelleType as Type, sum(nbDemiTable) as "Nombre de 1/2 tables"
 from(select numZone,numType from zone where zone.numEditeur is NULL) as temp,type,reservation
 where reservation.numZone=temp.numZone and temp.numType=type.numType group by libelleType
 */
