760834d0b390940e8a36b4512c61ae2926d27021
Gestin <gestin.remi@gmail.com>
2018-01-13 16:52:21 +0100

    demi table en table

--- application/views/Zone/liste_zone.php
+++ application/views/Zone/liste_zone.php
@@ -7,196 +7,160 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-   <title>Gentelella Alela! | </title>
+   <title> Projet Piscine! | </title>
 
-   <!-- Bootstrap -->
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-   <!-- Font Awesome -->
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-   <!-- NProgress -->
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/nprogress/nprogress.css" rel="stylesheet">
-   <!-- iCheck -->
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-   <!-- bootstrap-progressbar -->
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
-   <!-- JQVMap -->
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
-   <!-- bootstrap-daterangepicker -->
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
 
-   <!-- Custom Theme Style -->
-   <link href="<?php echo base_url() ?>gentelella-master/build/css/custom.min.css" rel="stylesheet">
+    <!-- bootstrap-progressbar -->
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <!-- bootstrap-daterangepicker -->
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
-   <!-- Datatables -->
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
-   <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
- </head>
+    <!-- Custom Theme Style -->
+    <link href="<?php echo base_url() ?>gentelella-master/build/css/custom.min.css" rel="stylesheet">
 
- <body class="nav-md">
-   <div class="container body">
-     <div class="main_container">
-       <div class="col-md-3 left_col">
-         <div class="left_col scroll-view">
-           <div class="navbar nav_title" style="border: 0;">
-             <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
-           </div>
+    <!-- Datatables -->
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>gentelella-master/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+  </head>
 
-           <div class="clearfix"></div>
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a class="site_title"><i class="fa fa-home"></i> <span>Festival du Jeu !</span></a>
+            </div>
 
-           <!-- menu profile quick info -->
-           <div class="profile clearfix">
+            <div class="clearfix"></div>
 
-             <div class="profile_info">
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              
+              <div class="profile_info">
+                <h4>ANNEE: <?php echo $this->session->anneeFestival ?></h4>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
 
-               <h4>ANNEE: <?php echo $this->session->anneeFestival ?></h4>
+            <br />
 
-             </div>
-           </div>
-           <!-- /menu profile quick info -->
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>Options</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-table"></i> Les differents tableaux <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="<?php echo site_url('Jeu'); ?>">Liste des Jeux</a></li>
+                      <li><a href="<?php echo site_url('Suivi'); ?>"> Suivi</a></li>
+                      <li><a href="<?php echo site_url('Zone'); ?>">Zone</a></li>
+                    </ul>
+                  </li>
+                 
+                  <li><a><i class="fa fa-wrench"></i> Gestion <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="<?php echo site_url('editeur'); ?>">Liste des Editeurs</a></li>
+                      <li><a href="<?php echo site_url('Admin'); ?>">Page Administrateur</a></li>
+                      
+                    </li>
+                 
+              </div>
+              
 
-           <br />
+            </div>
+            <!-- /sidebar menu -->
 
-           <!-- sidebar menu -->
-           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-             <div class="menu_section">
-               <h3>Options</h3>
-               <ul class="nav side-menu">
-                 <li><a><i class="fa fa-table"></i> Les differents tableaux <span class="fa fa-chevron-down"></span></a>
-                   <ul class="nav child_menu">
-                     <li><a href="<?php echo site_url('Jeu'); ?>">Liste des Jeux</a></li>
-                     <li><a href="<?php echo site_url('Suivi'); ?>"> Suivi</a></li>
-                     <li><a href="<?php echo site_url('Zone'); ?>">Zone</a></li>
-                   </ul>
-                 </li>
+            <!-- /menu footer buttons -->
+            <div class="sidebar-footer hidden-small">
+              
 
-                 <li><a><i class="fa fa-wrench"></i> Gestion <span class="fa fa-chevron-down"></span></a>
-                   <ul class="nav child_menu">
-                     <li><a href="<?php echo site_url('editeur'); ?>">Liste des Editeurs</a></li>
-                     <li><a href="<?php echo site_url('Admin'); ?>">Page Administrateur</a></li>
+              <a data-toggle="tooltip"  href="<?php echo site_url("utilisateur/deconnected") ?>">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+              </a>
+            </div>
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
 
-                   </li>
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav>
+            
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src alt=""><?php echo $this->session->login ?>
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="javascript:;"> Profile</a></li>
+                    
+                    <li><a href="<?php echo site_url("utilisateur/deconnected") ?>"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                  </ul>
+                </li>
 
-             </div>
-           </div>
-           <!-- /sidebar menu -->
-
-           <!-- /menu footer buttons -->
-           <div class="sidebar-footer hidden-small">
-             <a data-toggle="tooltip" data-placement="top" title="Settings">
-               <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-             </a>
-             <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-               <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-             </a>
-             <a data-toggle="tooltip" data-placement="top" title="Lock">
-               <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-             </a>
-             <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
-               <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-             </a>
-           </div>
-           <!-- /menu footer buttons -->
-         </div>
-       </div>
-
-       <!-- top navigation -->
-       <div class="top_nav">
-         <div class="nav_menu">
-           <nav>
-             <div class="nav toggle">
-               <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-             </div>
-
-             <ul class="nav navbar-nav navbar-right">
-               <li class="">
-                 <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                   <img src="images/img.jpg" alt=""><?php echo $login ?>
-                   <span class=" fa fa-angle-down"></span>
-                 </a>
-                 <ul class="dropdown-menu dropdown-usermenu pull-right">
-                   <li><a href="javascript:;"> Profile</a></li>
-                   <li>
-                     <a href="javascript:;">
-                       <span class="badge bg-red pull-right">50%</span>
-                       <span>Settings</span>
-                     </a>
-                   </li>
-                   <li><a href="javascript:;">Help</a></li>
-                   <li><a href="<?php echo site_url("utilisateur/deconnected") ?>"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                 </ul>
-               </li>
-
-               <li role="presentation" class="dropdown">
-                 <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-                   <i class="fa fa-envelope-o"></i>
-                   <span class="badge bg-green">6</span>
-                 </a>
-                 <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-                   <li>
-                     <a>
-                       <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                       <span>
-                         <span>John Smith</span>
-                         <span class="time">3 mins ago</span>
-                       </span>
-                       <span class="message">
-                         Film festivals used to be do-or-die moments for movie makers. They were where...
-                       </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a>
-                       <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                       <span>
-                         <span>John Smith</span>
-                         <span class="time">3 mins ago</span>
-                       </span>
-                       <span class="message">
-                         Film festivals used to be do-or-die moments for movie makers. They were where...
-                       </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a>
-                       <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                       <span>
-                         <span>John Smith</span>
-                         <span class="time">3 mins ago</span>
-                       </span>
-                       <span class="message">
-                         Film festivals used to be do-or-die moments for movie makers. They were where...
-                       </span>
-                     </a>
-                   </li>
-                   <li>
-                     <a>
-                       <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                       <span>
-                         <span>John Smith</span>
-                         <span class="time">3 mins ago</span>
-                       </span>
-                       <span class="message">
-                         Film festivals used to be do-or-die moments for movie makers. They were where...
-                       </span>
-                     </a>
-                   </li>
-                   <li>
-                     <div class="text-center">
-                       <a>
-                         <strong>See All Alerts</strong>
-                         <i class="fa fa-angle-right"></i>
-                       </a>
-                     </div>
-                   </li>
-                 </ul>
-               </li>
-             </ul>
-           </nav>
-         </div>
-       </div>
+                <li role="presentation" class="dropdown">
+                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-envelope-o"></i>
+                    <span class="badge bg-green">6  </span>
+                  </a>
+                  <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                    <li>
+                      <a>
+                        
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    
+                    
+                    <li>
+                      <a>
+                        
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="text-center">
+                        <a>
+                          <strong>See All Alerts</strong>
+                          <i class="fa fa-angle-right"></i>
+                        </a>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
        <!-- /top navigation -->
 <!--JE COMMENCE A MODIFIER ICI -->
 
@@ -208,22 +172,8 @@
              <div class="col-md-12 col-sm-12 col-xs-12">
                <div class="x_panel">
                  <div class="x_title">
-                   <h2>Editeurs <small>Users</small></h2>
-                   <ul class="nav navbar-right panel_toolbox">
-                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                     </li>
-                     <li class="dropdown">
-                       <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                       <ul class="dropdown-menu" role="menu">
-                         <li><a href="#">Settings 1</a>
-                         </li>
-                         <li><a href="#">Settings 2</a>
-                         </li>
-                       </ul>
-                     </li>
-                     <li><a class="close-link"><i class="fa fa-close"></i></a>
-                     </li>
-                   </ul>
+                   <h2>Editeurs </h2>
+                
                    <div class="clearfix"></div>
                  </div>
                  <div class="x_content">
@@ -234,7 +184,7 @@
                      <thead>
                        <tr>
                          <th>Nom Editeur</th>
-                         <th>Nb table</th>
+                         <th>Nb Demi table</th>
                        </tr>
                      </thead>
                        <?php
@@ -243,7 +193,7 @@ foreach ($zoneEditeur as $item) {
    echo <<<EOT
    <tr>
        <td><a href="$lien">$item->nomEditeur</td>
-       <td>$item->nbTable</td>
+       <td>$item->nbDemiTable</td>
 
     </tr>
 EOT;
@@ -256,7 +206,7 @@ EOT;
                      <thead>
                        <tr>
                          <th>Zones-types</th>
-                         <th>Nombre de table</th>
+                         <th>Nombre de Demi table</th>
                        </tr>
                      </thead>
                        <?php
@@ -264,7 +214,7 @@ foreach ($zoneType as $item) {
    echo <<<EOT
    <tr>
        <td>$item->libelleType</td>
-       <td>$item->nbTable</td>
+       <td>$item->nbDemiTable</td>
 
     </tr>
 EOT;
